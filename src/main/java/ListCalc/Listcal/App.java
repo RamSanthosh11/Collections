af2441787f5318523ccec18f992ceee2a356bebb
unknown <ramu3317@gmail.com>
2020-03-10 20:53:41 +0530

    first commit

---
+++ src/main/java/ListCalc/Listcal/App.java
@@ -0,0 +1,13 @@
+package ListCalc.Listcal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
